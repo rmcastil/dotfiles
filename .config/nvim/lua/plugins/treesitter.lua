34082bb51280822783a5365330de6776cd831cd9
Ryan Castillo <rmcastil@gmail.com>
2023-04-03 12:13:15 -0400

    Fix the help message we were seeing for treesitter

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ignore_install = {
+      "help",
+    },
+    ensure_installed = {
+      "bash",
+      "help",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "ruby",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+    },
+  },
+}
